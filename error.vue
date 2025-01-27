83c892b3f42f3c42d1bf339b0c420c9938522ca3
Riccardo Basso <riccardo@retebottega.it>
2025-01-27 09:25:53 +0100

    error 404 page

---
+++ error.vue
@@ -0,0 +1,17 @@
+<template>
+  <section class="error-container">
+    <div class="h-[50vh] flex items-center justify-center">
+      <img
+        src="https://www.riccardobasso.com/wp-content/uploads/2025/01/404-Error-bro.svg"
+        alt=" page not found"
+      />
+    </div>
+    <div class="mt-10">
+      <nuxt-link
+        to="/"
+        class="px-8 py-3 rounded-xl mt-4 inline-block bg-blu border-2 border-blu text-bianco hover:bg-transparent hover:text-blu transition-all duration-300"
+        >Homepage</nuxt-link
+      >
+    </div>
+  </section>
+</template>
